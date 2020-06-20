7159cd7f5b3610d6ef7fbc1fcaa361553001b11c
Rafał Wałach <62497415+rav250889@users.noreply.github.com>
2020-06-20 23:58:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,142 +1,41 @@
 $("document").ready(
     function()
     {
-        $("#protelnetClick").click(
-            function()
-            {
-                if($(this).text() === "+")
+        function showElements(showId, ClickId)
+        {
+             $("#"+ClickId).click(
+                function()
                 {
-                    $("#protelnet").fadeIn("slow");
-                
-                    $(this).text("-");
+                    if($(this).text() === "+")
+                    {
+                        $("#"+showId).fadeIn("slow");
+
+                        $(this).text("-");
+                    }
+
+                    else
+                    {
+                       $("#"+showId).hide();
+
+                        $(this).text("+"); 
+                    }
+
                 }
-                
-                else
-                {
-                   $("#protelnet").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
+            );
+
+        }
         
-        $("#hemClick").click(
-            function()
-            {
-                if($(this).text() === "+")
-                {
-                    $("#hem").fadeIn("slow");
-                
-                    $(this).text("-");
-                }
-                
-                else
-                {
-                   $("#hem").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
+        showElements("protelnet", "protelnetClick");
         
-        $("#msiClick").click(
-            function()
-            {
-                if($(this).text() === "+")
-                {
-                    $("#msi").fadeIn("slow");
-                
-                    $(this).text("-");
-                }
-                
-                else
-                {
-                   $("#msi").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
-        let flag = 0;
-        $(".click").click(
-            function()
-            {
-                if(flag === 0)
-                {
-                    $("#right").css({"animation-name": "anime"});
-                    flag = 1;
-                }
-                
-                else
-                {
-                    $("#right").css({"animation-name": "dwa"});
-                    flag = 0;
-                }
-            }
-        );
+        showElements("hem", "hemClick");
         
-        $("#langClick").click(
-            function()
-            {
-                if($(this).text() === "+")
-                {
-                    $("#lang").fadeIn(1000);
-                
-                    $(this).text("-");
-                }
-                
-                else
-                {
-                   $("#lang").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
+        showElements("msi", "msiClick");
         
-        $("#skillClick").click(
-            function()
-            {
-                if($(this).text() === "+")
-                {
-                    $("#skill").fadeIn(1000);
-                
-                    $(this).text("-");
-                }
-                
-                else
-                {
-                   $("#skill").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
+        showElements("lang", "langClick");
         
-        $("#intrestClick").click(
-            function()
-            {
-                if($(this).text() === "+")
-                {
-                    $("#intrest").fadeIn(1000);
-                
-                    $(this).text("-");
-                }
-                
-                else
-                {
-                   $("#intrest").hide();
-                
-                    $(this).text("+"); 
-                }
-                
-            }
-        );
+        showElements("skill", "skillClick");
+        
+        showElements("intrest", "intrestClick");
       
     }
 );
